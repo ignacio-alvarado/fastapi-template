c1d8440e4555ed0dc1ec263d2e0a41d76ebfda2a
ignacio-alvarado <ignacio.alvaradome@gmail.com>
2025-05-17 12:30:45 -0300

    Enhance logger configuration by adding a RotatingFileHandler.

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,6 +1,8 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
 logger = logging.getLogger(__name__)
+logger.addHandler(RotatingFileHandler('app.log', maxBytes=1024*1024, backupCount=5))
 
 def init_logger():
-    logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
